a9482d727d567dec7973a402c0db716bbdda0260
Kevin Anderson <kevin.anderso@gmail.com>
2024-11-05 09:11:45 -0500

    more path logging

--- pvlib/tests/conftest.py
+++ pvlib/tests/conftest.py
@@ -11,6 +11,7 @@ from functools import wraps
 import pvlib
 from pvlib.location import Location
 
+raise Exception(pvlib.__file__)
 
 pvlib_base_version = Version(Version(pvlib.__version__).base_version)
 
