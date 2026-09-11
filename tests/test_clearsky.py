a7e63a05aaddbd308f8c28c3e219f1ee204cc490
Kevin Anderson <kevin.anderso@gmail.com>
2026-09-11 14:27:20 +0200

    address numpy deprecation

--- tests/test_clearsky.py
+++ tests/test_clearsky.py
@@ -663,7 +663,7 @@ def test_detect_clearsky_arrays(detect_clearsky_data):
 def test_detect_clearsky_irregular_times(detect_clearsky_data):
     expected, cs = detect_clearsky_data
     times = cs.index.values.copy()
-    times[0] += 10**9
+    times[0] += pd.Timedelta(1, unit='s')
     times = pd.DatetimeIndex(times)
     with pytest.raises(NotImplementedError):
         clearsky.detect_clearsky(expected['GHI'].values, cs['ghi'].values,
